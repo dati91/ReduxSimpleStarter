179cb2e84dae70846e93ed6fb5618bdcaae51eba
Attila Dusnoki <dati91@gmail.com>
2018-03-10 22:19:21 +0100

    book list app

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Harry Potter', pages: 101 },
+        { title: 'Vulkan for dummies', pages: 1300 },
+        { title: 'The name of the wind', pages: 800 },
+        { title: 'Ready Player One', pages: 340 },
+    ]
+}
